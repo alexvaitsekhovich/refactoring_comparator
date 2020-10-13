6e7b75efd099808314eb85267185ca43fb3ae182
Alexander Vaitsekhovich <alex.vaitsekhovich@gmail.com>
2020-10-13 21:41:21 +0200

    Minor fixes

--- src/Logger/ConsoleLogger.php
+++ src/Logger/ConsoleLogger.php
@@ -8,7 +8,7 @@ class ConsoleLogger
 {
 	public static function log(string $message): void
 	{
-		if (!empty($_SERVER['CONSOLE_LOG'])) {
+		if (!empty($_SERVER['CONSOLE_LOG']) && ($_SERVER['CONSOLE_LOG']) ) {
 			echo $message . "\n";
 		}
 	}
